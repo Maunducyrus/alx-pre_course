c11133bed69bce41ba5f44c4861395eb590b179d
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-12 11:19:55 +0300

    commit of the day

---
+++ reactmedicalstorefrontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from'react-router-dom'
+
+import Login from './pages/Login'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <Router>
+        <Routes>
+            <Route exact path="/" element ={<Login/>} >
+            {/* <Route exact path="/about" component={About} /> */}
+            {/* <Route exact path="/contact" component={Contact} /> */}
+            </Route>
+        </Routes>
+    </Router>);
+
+// Create a root and render the component
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<h1>Hello World</h1>);
