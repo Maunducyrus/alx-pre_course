a4de8622884ed96f8c0f339599c67483ccc68e75
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-14 22:32:20 +0300

    className in login.js added

--- reactmedicalstorefrontend/src/pages/Login.js
+++ reactmedicalstorefrontend/src/pages/Login.js
@@ -34,45 +34,45 @@ class Login extends React.Component {
                 ]}
                 />
 
-                <div class="login-box">
+                <div className="login-box">
                     <div class="logo">
                         <a href="javascript:void(0);">Admin<b>BSB</b></a>
                         <small>Admin BootStrap Based - Material Design</small>
                     </div>
-                    <div class="card">
-                        <div class="body">
+                    <div className="card">
+                        <div className="body">
                             <form id="sign_in" method="POST" novalidate="novalidate">
-                                <div class="msg">Sign in to start your session</div>
-                                <div class="input-group">
-                                    <span class="input-group-addon">
-                                        <i class="material-icons">person</i>
+                                <div className="msg">Sign in to start your session</div>
+                                <div className="input-group">
+                                    <span className="input-group-addon">
+                                        <i className="material-icons">person</i>
                                     </span>
-                                    <div class="form-line">
-                                        <input type="text" class="form-control" name="username" placeholder="Username" required="" autofocus />
+                                    <div className="form-line">
+                                        <input type="text" className="form-control" name="username" placeholder="Username" required="" autofocus />
                                     </div>
                                 </div>
-                                <div class="input-group">
-                                    <span class="input-group-addon">
-                                        <i class="material-icons">lock</i>
+                                <div className="input-group">
+                                    <span className="input-group-addon">
+                                        <i className="material-icons">lock</i>
                                     </span>
-                                    <div class="form-line">
+                                    <div className="form-line">
                                         <input type="password" class="form-control" name="password" placeholder="Password" required />
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-xs-8 p-t-5">
+                                <div className="row">
+                                    <div className="col-xs-8 p-t-5">
                                         <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink" />
                                         <label for="rememberme">Remember Me</label>
                                     </div>
-                                    <div class="col-xs-4">
-                                        <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
+                                    <div className="col-xs-4">
+                                        <button className="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
                                     </div>
                                 </div>
-                                <div class="row m-t-15 m-b--20">
-                                    <div class="col-xs-6">
+                                <div className="row m-t-15 m-b--20">
+                                    <div className="col-xs-6">
                                         <a href="sign-up.html">Register Now!</a>
                                     </div>
-                                    <div class="col-xs-6 align-right">
+                                    <div className="col-xs-6 align-right">
                                         <a href="forgot-password.html">Forgot Password?</a>
                                     </div>
                                 </div>
