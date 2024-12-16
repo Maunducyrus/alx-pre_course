34cd404a7fd81ee07fd5504ad5bb3843f048dfa5
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-16 15:57:34 +0300

    npm install axios --legacy-peer-deps

--- reactmedicalstorefrontend/src/utils/AuthHandler.js
+++ reactmedicalstorefrontend/src/utils/AuthHandler.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class AuthHandler{
     static login (username, password, callback){
         axios.post(config.loginUrl, {username:username, password:password })
-        then(function(response){
+        .then(function(response){
             console.log(response);
         })
         .catch(function(error){
