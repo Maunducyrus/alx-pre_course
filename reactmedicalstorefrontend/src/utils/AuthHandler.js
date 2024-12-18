c5a2e0ef9fc8e474c58f2f937d52a07f62741719
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-18 23:21:28 +0300

    adding callbacks

--- reactmedicalstorefrontend/src/utils/AuthHandler.js
+++ reactmedicalstorefrontend/src/utils/AuthHandler.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import config from "./Config";
+import { reactLocalStorage } from 'reactjs-localstorage';
+
 
 class AuthHandler{
     static login (username, password, callback){
@@ -7,11 +9,13 @@ class AuthHandler{
         .then(function(response){
             // console.log(response);
             if(response.statusCode === 200){
-                
+                reactLocalStorage.set("refresh", response.data.refresh);
+                callback("Login successfully...");
             }
         })
         .catch(function(error){
             console.log(error.response);
+            callback("Error During Login...");
         });
     }
 }
