5af02c29c1b999882c5cc0d8abbdc64028ab6b9a
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-19 15:31:52 +0300

    created components folder

--- reactmedicalstorefrontend/src/index.js
+++ reactmedicalstorefrontend/src/index.js
@@ -10,7 +10,7 @@ root.render(
     <Router>
         <Routes>
             <Route exact path="/" element ={<Login/>} >
-            {/* <Route exact path="/about" component={About} /> */}
+            <Route exact path="/home" component={MainComponent} />
             {/* <Route exact path="/contact" component={Contact} /> */}
             </Route>
         </Routes>
