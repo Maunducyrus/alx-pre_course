aee46e6e8fd4ddbc3867b8b3a3a010416de5bf60
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-19 17:35:02 +0300

    added Navbar.js

---
+++ reactmedicalstorefrontend/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Narbar extends Component {
+    render() {
+        return <nav class="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
+                <a href="javascript:void(0);" class="bars" style="display: block;"></a>
+                <a class="navbar-brand" href="index.html">ADMINBSB - MATERIAL DESIGN</a>
+            </div>
+            </div>
+            </nav>
+    }
+}
+
+export default Narbar; 
