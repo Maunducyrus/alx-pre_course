b38e5053c81f97ca7ecf467feb233b24cb4ba5df
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-20 15:28:33 +0300

    jsx responsiveness

--- reactmedicalstorefrontend/src/components/Navbar.js
+++ reactmedicalstorefrontend/src/components/Navbar.js
@@ -6,18 +6,16 @@ class Narbar extends Component {
         return <nav className="navbar">
         <div className="container-fluid">
             <div className="navbar-header">
-                <a 
+                {/* <a 
                 href="#" 
                 className="navbar-toggle collapsed" 
                 data-toggle="collapse" 
                 data-target="#navbar-collapse" 
                 aria-expanded="false">
-                </a>
+                </a> */}
                 <a 
                 href="#"
-                className="bars" 
-                style= {{display: "none"}}>
-                </a>
+                className="bars" ></a>
                 <a className="navbar-brand" href="index.html">
                     ADMINBSB - MATERIAL DESIGN
                     </a>
