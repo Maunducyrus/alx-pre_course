0bdb64aa766b9a51b65a69d75c0ea8a05de15c86
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-20 13:00:57 +0300

    importation of files

--- reactmedicalstorefrontend/src/components/MainComponent.js
+++ reactmedicalstorefrontend/src/components/MainComponent.js
@@ -1,11 +1,17 @@
 // import React from "react";
 import React, { Component } from "react"; // Import both React and Component
 import Overlay from "./Overlay"; // Import the Overlay component
+import HomeComponets from "./HomeComponents"; // Import the Homecomponets
+import Navbar from "./Navbar"; //Import the Navbar component
+import Sidebar from "./Sidebar"; //Import the Sidebar component
 
 class MainComponent extends Component {
     render() {
         return <React.Fragment>
             <Overlay/>
+            <Navbar/>
+            <Sidebar/>
+            <HomeComponets/>
         </React.Fragment>
     }
 }
