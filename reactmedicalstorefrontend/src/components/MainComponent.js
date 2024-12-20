733f7415ccc7ec50c78041343e233c9734d4242f
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-20 13:36:47 +0300

    import GoogleFontLoader from 'react-google-font-loader';

--- reactmedicalstorefrontend/src/components/MainComponent.js
+++ reactmedicalstorefrontend/src/components/MainComponent.js
@@ -5,9 +5,30 @@ import HomeComponets from "./HomeComponents"; // Import the Homecomponets
 import Navbar from "./Navbar"; //Import the Navbar component
 import Sidebar from "./Sidebar"; //Import the Sidebar component
 
+import GoogleFontLoader from 'react-google-font-loader';
+
+
 class MainComponent extends Component {
     render() {
         return <React.Fragment>
+                <GoogleFontLoader
+                fonts={[
+                    {
+                        font: 'Roboto',
+                        weights: [400, 700],
+                    } 
+                ]}
+                subsets={['latin', 'cyrillic-ext']} 
+                />
+
+                <GoogleFontLoader
+                fonts={[
+                    {
+                    font: 'Material+Icons'            
+                    } 
+                ]}
+                />
+
             <Overlay/>
             <Navbar/>
             <Sidebar/>
