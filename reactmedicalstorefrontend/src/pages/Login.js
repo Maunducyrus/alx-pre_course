e5842f5b7da89e9bd2e02f4bb5cdfaadaac75d30
Maunducyrus <maunducyrus123@gmail.com>
2024-12-24 19:38:11 +0300

    adding Config.homeUrl to the AjaxHandler

--- reactmedicalstorefrontend/src/pages/Login.js
+++ reactmedicalstorefrontend/src/pages/Login.js
@@ -68,7 +68,7 @@ class Login extends React.Component {
         else{
             this.setState({loginStatus:3}); //successful response
             this.setState({ loginStatus: 3 });
-            window.location = 
+            window.location = config.homeUrl;
         }
     }
 
