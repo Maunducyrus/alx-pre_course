2e622e1c9a049072c4b3e4a0a70cedb8ae877b74
Maunducyrus <maunducyrus123@gmail.com>
2024-12-26 00:14:55 +0300

    ...

--- reactmedicalstorefrontend/src/index.js
+++ reactmedicalstorefrontend/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from'react-router-dom'
+import { PrivateRoute } from './utils/PrivateRoute';
 
 import Login from './pages/Login';
 import MainComponent from './components/MainComponent';
+import { PrivateRoute } from './utils/PrivateRoute';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,7 +13,7 @@ root.render(
     <Router>
         <Routes>
             <Route exact path="/" element ={<Login/>}></Route>
-            <Route exact path="/home" element={<MainComponent/>}></Route>
+            <PrivateRoute exact path="/home" element={<MainComponent/>}></PrivateRoute>
             
         </Routes>
     </Router>);
--- reactmedicalstorefrontend/src/utils/PrivateRoute.js
+++ reactmedicalstorefrontend/src/utils/PrivateRoute.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom"
 import AuthHandler from "./AuthHandler";
 
 export var PrivateRoute = ({component:Component,...rest}) =>{
+    console.log({ ...rest });
+    console.log(rest);
+    return (
     <Route
     {...rest}
-    render={(props) => AuthHandler.loggedIn() ? (<Component {...props}/>) : <Redirect to='/' />} />
+    render={(props) => 
+        AuthHandler.loggedIn() ? (<Component {...props}/>) : <Redirect to='/' />} 
+        />
+    );
 }
