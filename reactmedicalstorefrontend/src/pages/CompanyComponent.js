25ac648f566429ed0f19a244a9074ba2b9b1216b
Maunducyrus <maunducyrus123@gmail.com>
2024-12-26 00:38:01 +0300

    CompanyComponent content

--- reactmedicalstorefrontend/src/pages/CompanyComponent.js
+++ reactmedicalstorefrontend/src/pages/CompanyComponent.js
@@ -1,66 +1,16 @@
 // import React from 'react';
 import React, { Component } from 'react';
 
-class HomeComponets extends Component {
+class CompanyComponent extends Component {
     render() {
         return <section className="content">
         <div className="container-fluid">
             <div className="block-header">
-                <h2>DASHBOARD</h2>
+                <h2>MANAGE COMPANY</h2>
             </div>
-
-            <div className="row clearfix">
-                <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box bg-pink hover-expand-effect">
-                        <div className="icon">
-                            <i className="material-icons">playlist_add_check</i>
-                        </div>
-                        <div className="content">
-                            <div className="text">NEW TASKS</div>
-                            <div className="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20">125</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box bg-cyan hover-expand-effect">
-                        <div className="icon">
-                            <i className="material-icons">help</i>
-                        </div>
-                        <div className="content">
-                            <div className="text">NEW TICKETS</div>
-                            <div className="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">257</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box bg-light-green hover-expand-effect">
-                        <div className="icon">
-                            <i className="material-icons">forum</i>
-                        </div>
-                        <div className="content">
-                            <div className="text">NEW COMMENTS</div>
-                            <div className="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20">243</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box bg-orange hover-expand-effect">
-                        <div className="icon">
-                            <i className="material-icons">person_add</i>
-                        </div>
-                        <div className="content">
-                            <div className="text">NEW VISITORS</div>
-                            <div className="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20">1225</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* <!-- #END# Widgets -->
-            <!-- CPU Usage --> */}
-              
         </div>
         </section>
     }
 }
 
-export default HomeComponets;
+export default CompanyComponent;
