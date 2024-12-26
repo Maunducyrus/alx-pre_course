b1eb5f989913562bee029d97d94d0b0cb5c6982b
Maunducyrus <maunducyrus123@gmail.com>
2024-12-26 23:11:11 +0300

    import Maincomponent importing the new PrivateRoute

--- reactmedicalstorefrontend/src/utils/PrivateRouteNew.js
+++ reactmedicalstorefrontend/src/utils/PrivateRouteNew.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom"
 import AuthHandler from "./AuthHandler";
+import MainComponent from "../components/MainComponent"
 
 export var PrivateRouteNew = ({ page }) =>{
     return (
     <Route
-    render={(props) => 
-        AuthHandler.loggedIn() ? <MainComponent /> : <Redirect to='/' />} 
-        />
+    render={() => 
+        AuthHandler.loggedIn() ? <MainComponent page={page}/> : <Redirect to='/' />
+    } 
+    />
     );
 }
