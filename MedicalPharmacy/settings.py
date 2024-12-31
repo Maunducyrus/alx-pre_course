6cd3630e6531484c2eae7a58c947995f243d1843
Maunducyrus <maunducyrus123@gmail.com>
2024-12-31 19:08:48 +0300

    settings.py

--- MedicalPharmacy/settings.py
+++ MedicalPharmacy/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'application',
     'rest_framework',
-    'rest_framework_simplejwt',
+    # 'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'corsheaders'
 ]
