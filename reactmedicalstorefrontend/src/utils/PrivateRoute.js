c9e2ec452f4c2c30f1e075a59b60bf80220835d3
Maunducyrus <maunducyrus123@gmail.com>
2024-12-31 21:44:02 +0300

    ...

--- reactmedicalstorefrontend/src/utils/PrivateRoute.js
+++ reactmedicalstorefrontend/src/utils/PrivateRoute.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { Route, Redirect, Navigate } from "react-router-dom"
+import { Route, Navigate } from "react-router-dom"
 import AuthHandler from "./AuthHandler";
 
-export var PrivateRoute = ({component:Component,...rest}) =>{
+// export var PrivateRoute = ({component:Component,...rest}) =>{
+//     console.log({ ...rest });
+//     console.log(rest);
+//     return (
+//     <Route
+//     {...rest}
+//     element={(props) => 
+//         AuthHandler.loggedIn() ? (<Component {...props}/>) : <Navigate to="/" />} 
+//         />
+//     );
+// }
+export var PrivateRoute = ({ component: Component, ...rest }) => {
     console.log({ ...rest });
     console.log(rest);
     return (
-    <Route
-    {...rest}
-    element={(props) => 
-        AuthHandler.loggedIn() ? (<Component {...props}/>) : <Navigate to="/" />} 
+        <Route
+            {...rest}
+            element={
+                AuthHandler.loggedIn() 
+                    ? <Component {...rest} /> 
+                    : <Navigate to="/" />
+            }
         />
     );
-}
+};
