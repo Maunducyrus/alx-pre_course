6ebb1f8d7857a56126f5de8dc3986fa12590ef34
Maunducyrus <maunducyrus123@gmail.com>
2025-01-12 19:15:10 +0300

    PrivateRouteNew.js

--- reactmedicalstorefrontend/src/utils/PrivateRouteNew.js
+++ reactmedicalstorefrontend/src/utils/PrivateRouteNew.js
@@ -28,22 +28,22 @@
 
 // export default PrivateRouteNew;
 
-import React from "react";
-import { Route, Navigate } from "react-router-dom";
-import AuthHandler from "./AuthHandler";
-import MainComponent from "../components/MainComponent";
+// import React from "react";
+// import { Route, Navigate } from "react-router-dom";
+// import AuthHandler from "./AuthHandler";
+// import MainComponent from "../components/MainComponent";
 
-export var PrivateRouteNew = ({ page, activepage, ...rest }) => {
-    return (
-        <Route
-            {...rest}
-            element={
-                AuthHandler.loggedIn() 
-                    ? <MainComponent page={page} activepage={activepage} />
-                    : <Navigate to="/" />
-            }
-        />
-    );
-};
+// export var PrivateRouteNew = ({ page, activepage, ...rest }) => {
+//     return (
+//         <Route
+//             {...rest}
+//             element={
+//                 AuthHandler.loggedIn() 
+//                     ? <MainComponent page={page} activepage={activepage} />
+//                     : <Navigate to="/" />
+//             }
+//         />
+//     );
+// };
 
-export default PrivateRouteNew;
+// export default PrivateRouteNew;
