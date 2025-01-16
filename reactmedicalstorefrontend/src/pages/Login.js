157237ba1aa1b886ce6695be78c815e24d0b8ac5
Maunducyrus <maunducyrus123@gmail.com>
2025-01-16 14:31:17 +0300

    ...

--- reactmedicalstorefrontend/src/pages/Login.js
+++ reactmedicalstorefrontend/src/pages/Login.js
@@ -1,198 +1,177 @@
-import React, { useCallback } from 'react';
-import 'adminbsb-materialdesign/plugins/bootstrap/css/bootstrap.css'
-import 'adminbsb-materialdesign/plugins/node-waves/waves.css';
-import 'adminbsb-materialdesign/plugins/animate-css/animate.css';
-// import 'adminbsb-materialdesign/plugins/bootstrap/css/bootstrap.css'
-import 'adminbsb-materialdesign/css/style.css'
-import GoogleFontLoader from 'react-google-font-loader';
-import AuthHandler from '../utils/AuthHandler';
-import config from '../utils/Config';
-import { Navigate } from 'react-router-dom';
-import { reactLocalStorage } from 'reactjs-localstorage';
-
-
-
+import React from "react";
+import GoogleFontLoader from "react-google-font-loader";
+import "adminbsb-materialdesign/plugins/bootstrap/css/bootstrap.css";
+import "adminbsb-materialdesign/plugins/node-waves/waves.css";
+import "adminbsb-materialdesign/plugins/animate-css/animate.css";
+import "adminbsb-materialdesign/css/style.css";
+import AuthHandler from "../utils/AuthHandler";
+import { reactLocalStorage } from "reactjs-localstorage";
+import { Navigate } from "react-router-dom";
+import Config from "../utils/Config";
 
 class Login extends React.Component {
-    // constructor(props) {
-    //     super(props);
-
-        // Bind methods to 'this'
-    //     this.saveInputs = this.saveInputs.bind(this);
-    //     this.formSubmit = this.formSubmit.bind(this);
-    // }
-
-    state = {
-        username: "",
-        password: "",
-        btnDisabled: true,
-        loginStatus: 0,
+  state = {
+    username: "",
+    password: "",
+    btnDisabled: true,
+    loginStatus: 0,
+  };
+  saveInputs = (event) => {
+    var key = event.target.name;
+    this.setState({ [key]: event.target.value });
+    if (this.state.username != "" && this.state.password != "") {
+      this.setState({ btnDisabled: false });
+    } else {
+      this.setState({ btnDisabled: true });
     }
+  };
 
-    saveInputs(event) {
-        var key=event.target.name;
-        this.setState({ [key]: event.target.value});
-        if(this.state.username != "" && this.state.password != "") {
-            this.setState({btnDisabled: false});
-        } 
-        else {
-            this.setState({btnDisabled: true});
-        }
-    };
-    // saveInputs(event) {
-    //     const key = event.target.name;
-    //     const value = event.target.value;
-    //     this.setState({ [key]: value }, () => {
-    //         this.setState({
-    //             btnDisabled: !(this.state.username && this.state.password),
-    //         });
-    //     });
-    // }
+  formSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    this.setState({ loginStatus: 1 });
+    AuthHandler.login(
+      this.state.username,
+      this.state.password,
+      this.handleAjaxResponse
+    );
+  };
 
-    formSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-        this.setState({ loginStatus: 1}); // For demonstration purposes, simulating an asynchronous request.
-        AuthHandler.login(
-            this.state.username,
-            this.state.password,
-            this.handleAjaxResponse,
-        );
-    };
-
-    handleAjaxResponse = (data) => {
-        console.log(data);
-        if(data.error){
-            this.setState({loginStatus: 4}); // Simulating an error response.
-            // this.setState({ loginStatus: 4 });
-        }
-        else{
-            this.setState({loginStatus:3}); //successful response
-            // this.setState({ loginStatus: 3 });
-            window.location = config.homeUrl;
-        }
-    };
-
-    getMessages = () =>{
-        if (this.state.loginStatus === 0){
-            return "";
-        }
-        // else if(this.state.loginStatus=== 1){
-        //     return (
-        //         <div class="alert alert-warning">
-        //     <strong>Logging in!</strong> Please Wait...</div>
-        //     );
-        // }
-        else if(this.state.loginStatus===1){
-            return (
-                <div class="alert alert-success">
-            <strong>Login successfully!</strong> </div>
-            );
-        }
-        else if(this.state.loginStatus=== 4){
-            return (
-                <div class="alert alert-danger">
-            <strong>Invalid Login Details</strong> 
-                </div>
-            );
-        }
-    };
-    render() {
-        if (AuthHandler.loggedIn()){
-            return <Navigate to = {config.homeUrl} />;
-        }
-        document.body.className = "login-page";
-
-        return (
-                <React.Fragment>
-                <GoogleFontLoader
-                fonts={[
-                    {
-                        font: 'Roboto',
-                        weights: [400, 700],
-                    } 
-                ]}
-                subsets={['latin', 'cyrillic-ext']} 
-                />
-
-                <GoogleFontLoader
-                fonts={[
-                    {
-                    font: 'Material+Icons',          
-                    },
-                ]}
-                />
-
-                <div className ="login-box">
-                    <div className="logo">
-                        <a href="javascript:void(0);">Medical Store Management System</a>
-                        {/* <small>Medical Store Management System</small> */}
-                    </div>
-                    <div className="card">
-                        <div className="body">
-                            <form id="sign_in" method="POST" onSubmit={this.formSubmit}>
-                                <div className="msg">Sign in</div>
-                                <div className="input-group">
-                                    <span className="input-group-addon">
-                                        <i className="material-icons">person</i>
-                                    </span>
-                                    <div className="form-line">
-                                        <input 
-                                        type="text" 
-                                        className="form-control" 
-                                        name="username" 
-                                        placeholder="Username" 
-                                        required
-                                        autofocus 
-                                        onChange={this.saveInputs}
-
-                                        />
-                                    </div>
-                                </div>
-                                <div className="input-group">
-                                    <span className="input-group-addon">
-                                        <i className="material-icons">lock</i>
-                                    </span>
-                                    <div className="form-line">
-                                        <input 
-                                        type="password" 
-                                        className="form-control" 
-                                        name="password" 
-                                        placeholder="Password" 
-                                        required 
-                                        onChange={this.saveInputs}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-xs-8 p-t-5">
-                                        <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink" />
-                                        <label for="rememberme">Remember Me</label>
-                                    </div>
-                                    <div className="col-xs-4">
-                                        <button className="btn btn-block bg-pink waves-effect" 
-                                        type="submit"
-                                        disabled={this.state.btnDisabled}
-                                        >SIGN IN</button>
-                                    </div>
-                                </div>
-                                <div className="row m-t-15 m-b--20">
-                                    <div className="col-xs-6">
-                                        <a href="sign-up.html">Register Now!</a>
-                                    </div>
-                                    <div className="col-xs-6 align-right">
-                                        <a href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div className="col-xs-12"> {this.getMessages()}</div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                </React.Fragment>
-        );
+  handleAjaxResponse = (data) => {
+    console.log(data);
+    if (data.error) {
+      this.setState({ loginStatus: 4 });
+    } else {
+      this.setState({ loginStatus: 3 });
+      window.location = Config.homeUrl;
     }
+  };
 
+  getMessages = () => {
+    if (this.state.loginStatus === 0) {
+      return "";
+    } 
+    // else if (this.state.loginStatus === 1) {
+    //   return (
+    //     <div class="alert alert-warning">
+    //       <strong>Logging in!</strong> Please Wait...
+    //     </div>
+    //   );
+    // }
+     else if (this.state.loginStatus === 3) {
+      return (
+        <div class="alert alert-success">
+          <strong>Login Successfull!</strong>
+        </div>
+      );
+    } else if (this.state.loginStatus === 4) {
+      return (
+        <div class="alert alert-danger">
+          <strong>Invalid Login Details</strong>
+        </div>
+      );
+    }
+  };
+
+  render() {
+    if (AuthHandler.loggedIn()) {
+      return <Navigate to={Config.homeUrl} />;
+    }
+    document.body.className = "login-page";
+
+    return (
+      <React.Fragment>
+        <GoogleFontLoader
+          fonts={[
+            {
+              font: "Roboto",
+              weights: [400, 700],
+            },
+          ]}
+          subsets={["latin", "cyrillic-ext"]}
+        />
+        <GoogleFontLoader
+          fonts={[
+            {
+              font: "Material+Icons",
+            },
+          ]}
+        />
+        <div className="login-box">
+          <div className="logo">
+            <a href="javascript:void(0);">Medical Store Management System</a>
+          </div>
+          <div className="card">
+            <div className="body">
+              <form id="sign_in" method="POST" onSubmit={this.formSubmit}>
+                <div className="msg">Sign in</div>
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <i className="material-icons">person</i>
+                  </span>
+                  <div className="form-line">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="username"
+                      placeholder="Username"
+                      required
+                      autofocus
+                      onChange={this.saveInputs}
+                    />
+                  </div>
+                </div>
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <i className="material-icons">lock</i>
+                  </span>
+                  <div className="form-line">
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      placeholder="Password"
+                      required
+                      onChange={this.saveInputs}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-xs-8 p-t-5">
+                    <input
+                      type="checkbox"
+                      name="rememberme"
+                      id="rememberme"
+                      className="filled-in chk-col-pink"
+                    />
+                    <label for="rememberme">Remember Me</label>
+                  </div>
+                  <div className="col-xs-4">
+                    <button
+                      className="btn btn-block bg-pink waves-effect"
+                      type="submit"
+                      disabled={this.state.btnDisabled}
+                    >
+                      SIGN IN
+                    </button>
+                  </div>
+                </div>
+                <div className="row m-t-15 m-b--20">
+                  <div className="col-xs-6">
+                    <a href="sign-up.html">Register Now!</a>
+                  </div>
+                  <div className="col-xs-6 align-right">
+                    <a href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div className="col-xs-12">{this.getMessages()}</div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Login;
